4a548f8faf383016ccf0861876312e364fd57004
Tom Rutherford <tmrutherford@gmail.com>
2022-11-26 16:13:09 -0700

    Add test_get_platform_to_version

--- test/test_main.py
+++ test/test_main.py
@@ -5,8 +5,9 @@ from unittest import mock
 
 import pytest
 import responses
+from packaging.version import Version
 
-from main import EOL_PYTHON_VERSIONS_URL, GHA_PYTHON_VERSIONS_URL, main
+from main import EOL_PYTHON_VERSIONS_URL, GHA_PYTHON_VERSIONS_URL, Platform, get_platform_to_version, main
 
 data = [
     [('3.4', 'latest', 'false'), ['3.9.6', '3.8.11', '3.7.11', '3.6.14', '3.5.10', '3.4.10']],
@@ -42,3 +43,50 @@ def test_main_without_max_version(capsys, args, result, github_env):
     result_json = json.dumps(result)
     assert env.read_text() == f'LATEST_PYTHON_VERSIONS={result_json}'
     assert output.read_text() == f'latest-python-versions={result_json}'
+
+
+def test_get_platform_to_version():
+    data = [
+        {
+            'version': '3.10.0-beta.2',
+            'files': [
+                {'arch': 'x64', 'platform': 'darwin'},
+                {'arch': 'x64', 'platform': 'linux', 'platform_version': '20.04'},
+                {'arch': 'x64', 'platform': 'win32'},
+            ],
+        },
+        {
+            'version': '3.6.7',
+            'files': [
+                {'arch': 'x64', 'platform': 'darwin'},
+                {'arch': 'x64', 'platform': 'linux', 'platform_version': '20.04'},
+                {'arch': 'x86', 'platform': 'win32'},
+            ],
+        },
+    ]
+    r = get_platform_to_version(data, min_version=Version('3.0'), max_version=Version('4.0'), include_prereleases=True)
+    assert r == {
+        Platform(name='darwin', arch='x64', version=''): {Version('3.6.7'), Version('3.10.0b2')},
+        Platform(name='linux', arch='x64', version='20.04'): {Version('3.6.7'), Version('3.10.0b2')},
+        Platform(name='win32', arch='x64', version=''): {Version('3.10.0b2')},
+        Platform(name='win32', arch='x86', version=''): {Version('3.6.7')},
+    }
+    r = get_platform_to_version(data, min_version=Version('3.8'), max_version=Version('4.0'), include_prereleases=True)
+    assert r == {
+        Platform(name='darwin', arch='x64', version=''): {Version('3.10.0b2')},
+        Platform(name='linux', arch='x64', version='20.04'): {Version('3.10.0b2')},
+        Platform(name='win32', arch='x64', version=''): {Version('3.10.0b2')},
+    }
+
+    r = get_platform_to_version(data, min_version=Version('3.0'), max_version=Version('3.8'), include_prereleases=True)
+    assert r == {
+        Platform(name='darwin', arch='x64', version=''): {Version('3.6.7')},
+        Platform(name='linux', arch='x64', version='20.04'): {Version('3.6.7')},
+        Platform(name='win32', arch='x86', version=''): {Version('3.6.7')},
+    }
+    r = get_platform_to_version(data, min_version=Version('3.0'), max_version=Version('4.0'), include_prereleases=False)
+    assert r == {
+        Platform(name='darwin', arch='x64', version=''): {Version('3.6.7')},
+        Platform(name='linux', arch='x64', version='20.04'): {Version('3.6.7')},
+        Platform(name='win32', arch='x86', version=''): {Version('3.6.7')},
+    }
