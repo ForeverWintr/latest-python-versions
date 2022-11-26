669cb63b0ada61e9870377750b0e40371ecb6c09
Tom Rutherford <tmrutherford@gmail.com>
2022-11-26 12:15:03 -0700

    Trivial commit to launch workflows

--- test/test_main.py
+++ test/test_main.py
@@ -38,6 +38,7 @@ def test_main_without_max_version(capsys, args, result, github_env):
     captured = capsys.readouterr()
     assert json.loads(captured.out) == result
 
+    print('run workflows')
     env, output = github_env
     result_json = json.dumps(result)
     assert env.read_text() == f'LATEST_PYTHON_VERSIONS={result_json}'
